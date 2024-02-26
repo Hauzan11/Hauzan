4822d6bc61713003d96e36b0f8346d51b840a11f
Hauzan Aflah Asshidqi <Hauzanaflah01@gmail.com>
2024-02-26 15:26:59 +0700

    UKK BONET SMK AL HAFIDZ LEUWILIANG

---
+++ application/views/frontend/v_homepage.php
@@ -0,0 +1,287 @@
+  <!--/ Intro Skew Star /-->
+  <div id="home" class="intro route bg-image" style="background-image: url(<?php echo base_url(); ?>assets_frontend/img/latar1-bg.png)">
+    <div class="overlay-itro"></div>
+    <div class="intro-content display-table">
+      <div class="table-cell">
+        <div class="container">
+          <!--<p class="display-6 color-d">Hello, world!</p>-->
+          <h1 class="intro-title mb-4"><?php echo $pengaturan->nama ?></h1>
+          <p class="intro-subtitle"><span class="text-slider-items">Selamat Datang di Portal Informasi Sekolah <?php echo $pengaturan->nama ?> </span><strong class="text-slider"></strong></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--/ Intro Skew End /-->
+
+  <br/>
+  <br/>
+  <br/>
+
+  <!--/ Section Services Star /-->
+  <section id="service" class="services-mf route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              Visi dan Misi
+            </h3>
+            <p class="subtitle-a">
+              SMK Al hafidz leuwiliang
+            </p>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-stats-bars"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">VISI</h2>
+              <p class="s-description text-center">
+                Mewujudkan generasi yang bermartabat, terampil dan unggul dalam IMTAQ (Iman dan Taqwa) dan IPTEK (Ilmu pengetahuan dan Teknologi)
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-connection-bars"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">MISI</h2>
+              <p class="s-description text-center">
+                <p> 1. Melaksanakan pembelajaran dan pelatihan yang berwawasan mutu dan keunggulan. </p>
+                <p> 2. Mempersiapakan perserta didik menjadi golongan ummat yang uswatun khasanah. </p>
+                <p> 3. Menumbuh kembangkan kehidupan yang islami berdasarkan syariat islam kaffah. </p>
+                <p> 4. Mengembangkan kemitraan dengan DU/DI dan instansi lain secara sinegris. </p>
+                <p> 5. Modernisasi sarana dan prasarana sekolah. </p>
+                <p> 6. Meningkatan sistem informasi dan layanan sekolah. </p>
+                <p> 7. Meningkatan profesionalisme dan kerjasama yang baik antar pendidik dan tenaga kependidikan. </p>
+                <p> 8. Meningkatkan kompetensi lulusan yang siap untuk melanjutkan,bekerja dan berwirausaha. </p>
+                <p> 9. Mengembangkan potensi peserta didik melalui berbagai kegiatan ekstrakurikuler dan pembinaan karakter. </p>
+                <p> 10. Meningkatkan prestasi akademik dan non akademik melalaui proses seleksi, pembinaan dan aktif mengirimkan delegasi dalam berbagai ajang kejuaraan. </p>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-box">
+            <div class="service-ico">
+              <span class="ico-circle"><i class="ion-android-checkbox"></i></span>
+            </div>
+            <div class="service-content">
+              <h2 class="s-title">Tujuan</h2>
+              <p class="s-description text-center">
+              <p> 1. Mewujudkan SDM yang cerdas, intelektual, spiritual, emosional, kinestetik dan produktif yang dilandasi ahlak mulia. </p>
+              <p> 2. Menyiapkan SDM yang berkualitas untuk memasuki studi lanjut, dunia kerja, dan beradaptasi dengan lingkungan. </p>
+              <p> 3. Menyiapkan individu muslim yang kaffah sehingga terhindar dari asfala safiliin (tempat yang serendah rendahnya). </p>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ Section Services End /-->
+
+  <div class="section-counter paralax-mf bg-image" style="background-image: url(img/belajar-smp-bg.jpg)">
+    <div class="overlay-mf"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-person-stalker"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">520</p>
+              <span class="counter-text">JUMLAH SISWA</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box pt-4 pt-md-0">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-university"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">35</p>
+              <span class="counter-text">JUMLAH GURU DAN TENAGA KEPENDIDIKAN</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box pt-4 pt-md-0">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-ios-people"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">18</p>
+              <span class="counter-text">JUMLAH ROMBONGAN BELAJAR</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-lg-3">
+          <div class="counter-box pt-4 pt-md-0">
+            <div class="counter-ico">
+              <span class="ico-circle"><i class="ion-ios-home"></i></span>
+            </div>
+            <div class="counter-num">
+              <p class="counter">15</p>
+              <span class="counter-text">JUMLAH SARANA & PRASARANA</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!--/ Section Portfolio Star /-->
+  <section id="work" class="portfolio-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              FASILITAS DAN SARANA PENUNJANG LAINNYA
+            </h3>
+            <p class="subtitle-a">
+              SMK AL HAFIDZ LEUWILIANG
+            </p>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="work-box">
+            <a href="<?php echo base_url() ?>gambar/website/Lab.jpg" data-lightbox="gallery-mf">
+              <div class="work-img">
+                <img src="<?php echo base_url(); ?>gambar/website/lab.jpg" alt="" class="img-fluid">
+              </div>
+              <div class="work-content">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <h2 class="w-title">Lab. Komputer</h2>
+                    <div class="w-more">
+                      <span class="w-ctegory">Hauzan aflah asshidqi</span> / <span class="w-date">12 Feb. 2024</span>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="w-like">
+                      <span class="ion-ios-plus-outline"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="work-box">
+            <a href="<?php echo base_url() ?>gambar/website/belajar.jpeg" data-lightbox="gallery-mf">
+              <div class="work-img">
+                <img src="<?php echo base_url(); ?>gambar/website/belajar.jpeg" alt="" class="img-fluid">
+              </div>
+              <div class="work-content">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <h2 class="w-title">Ruangan belajar</h2>
+                    <div class="w-more">
+                      <span class="w-ctegory">Hauzan aflah asshidqi</span> / <span class="w-date">12 Feb. 2024</span>
+                    </div>
+                  </div>
+                  <div class="col-sm-4">
+                    <div class="w-like">
+                      <span class="ion-ios-plus-outline"></span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="work-box">
+            <a href="<?php echo base_url() ?>gambar/website/masjid.jpeg" data-lightbox="gallery-mf">
+              <div class="work-img">
+                <img src="<?php echo base_url(); ?>gambar/website/masjid.jpeg" alt="" class="img-fluid">
+              </div>
+              <div class="work-content">
+                <div class="row">
+                  <div class="col-sm-8">
+                    <h2 class="w-title">Musholla</h2>
+                    <div class="w-more">
+                      <span class="w-ctegory">Hauzan aflah asshidqi</span> / <span class="w-date">12 Feb. 2024</span>
+                    </div>
+                  </div>
+                  
+        
+      </div>
+    </div>
+  </section>
+  <!--/ Section Portfolio End /-->
+
+  <!--/ Section Testimonials Star /-->
+  
+
+  <!--/ Section Blog Star /-->
+  <section id="blog" class="blog-mf sect-pt4 route">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="title-box text-center">
+            <h3 class="title-a">
+              Gallery
+            </h3>
+            <p class="subtitle-a">
+              Postingan Terbaru Dari Kami.
+            </p>
+            <div class="line-mf"></div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <?php foreach($artikel as $a){ ?>
+          <div class="col-md-4">
+            <div class="card card-blog">
+              <div class="card-img">
+                <a href="<?php echo base_url().$a->artikel_slug ?>">
+                  <?php if($a->artikel_sampul != ""){ ?>
+                    <img src="<?php echo base_url(); ?>gambar/artikel/<?php echo $a->artikel_sampul ?>" alt="" class="img-fluid">
+                  <?php } ?>
+                </a>
+              </div>
+              <div class="card-body">
+                <div class="card-category-box">
+                  <div class="card-category">
+                    <h6 class="category"><?php echo $a->kategori_nama ?></h6>
+                  </div>
+                </div>
+
+                <h3 class="card-title"><a href="<?php echo base_url().$a->artikel_slug ?>"><?php echo $a->artikel_judul ?></a></h3>
+
+              </div>
+              <div class="card-footer">
+                <div class="post-author">
+                  <a href="#">
+                    <span class="author"><?php echo $a->pengguna_nama; ?></span>
+                  </a>
+                </div>
+                <div class="post-date">
+                  <span class="ion-ios-clock-outline"></span> <?php echo date('d-M-Y', strtotime($a->artikel_tanggal)); ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+        
+      </div>
+    </div>
+  </section>
+  <!--/ Section Blog End /-->
